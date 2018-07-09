3a90138a59d32f521ca24799aacd65c30309bd33
rubenfig <shishi9tail@hotmail.com>
2018-07-09 15:34:01 -0400

    Cambios en el package.json y nombre de la cuenta

--- src/android/AccountGeneral.java
+++ src/android/AccountGeneral.java
@@ -1,18 +1,13 @@
 package com.rubenfig.plugin.contacts;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Udini
- * Date: 20/03/13
- * Time: 18:11
- */
+
 public class AccountGeneral {
 
     /**
      * Account type id
      */
     public static final String ACCOUNT_TYPE = "com.rubenfig.plugin.contacts";
-    public static final String ACCOUNT_NAME = "sample";
+    public static final String ACCOUNT_NAME = "Contactos";
     public static final String ACCOUNT_TOKEN = "12345";
     /**
      * Account name
@@ -22,9 +17,9 @@ public class AccountGeneral {
      * Auth token types
      */
     public static final String AUTHTOKEN_TYPE_READ_ONLY = "Read only";
-    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an Udinic account";
+    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an account";
 
     public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
-    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an Udinic account";
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an account";
 
 }
